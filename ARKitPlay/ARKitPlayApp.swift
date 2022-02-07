830ad081585415c38f7ea2b4e9be50b64edc825c
Jong Yoon Kim <jyoon95@gmail.com>
2022-02-07 11:18:51 +0900

    Initial Commit

---
+++ ARKitPlay/ARKitPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitPlayApp.swift
+//  ARKitPlay
+//
+//  Created by Jong Yoon Kim on 2022/02/07.
+//
+
+import SwiftUI
+
+@main
+struct ARKitPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
